3be6ffb144824932d94709f6d65635175c0aec0e
Esteban Borai <estebanborai@outlook.com>
2019-04-02 11:30:37 -0300

    Chapter 4 Samples

---
+++ htp/samples/ch04/4.19/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int c;
+
+  c = 5;
+  cout << c << endl;
+  cout << c++ << endl;
+  cout << c << endl;
+
+  cout << endl;
+
+  c = 5;
+  cout << c << endl;
+  cout << ++c << endl;
+  cout << c << endl;
+}
