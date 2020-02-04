6821e9500ab9013dd07c83eb7d15faaf1a26ecb6
Esteban Borai <estebanborai@gmail.com>
2020-02-04 08:25:43 -0300

    Add exercise C-1-6.cpp

--- exercises/ch1/C-1-6.cpp
+++ exercises/ch1/C-1-6.cpp
@@ -1,32 +1,15 @@
+#include <algorithm>
+#include <cstdio>
 #include <iostream>
 
-/**
- * factorial calculates the factorial value of "n"
- * 
- */
-float factorial(float n) {
-  int i = 1;
-  float factorial = 1;
-
-  for (i; i <= int(n); i++) {
-    factorial=factorial * i;
-  }
-
-  return factorial;
-}
-
-/**
- * combinations calculates the total existent combinations
- * of a given total of elements and an expected set
- * to combine
- * 
- */
-int combinations(int totalElements, int objects) {
-  return int(factorial(totalElements)/(factorial(objects) * factorial(totalElements - objects)));
-}
-
 int main() {
-  std::cout << combinations(6, 6) << std::endl;
+  char string[] = "abcdef";
+  int totalPermutations = 0;
 
-  return 0;
+  do {
+    std::puts(string);
+    totalPermutations++;
+  } while (std::next_permutation(string, string + sizeof(string) - 1));
+
+  std::cout << "\nTotal Permutations: " << totalPermutations << std::endl;
 }
